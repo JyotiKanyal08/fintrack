20f150700381392bfae7e02d3d8ad294c7b90f25
JyotiKanyal08 <jyotikanyal48@gmail.com>
2026-07-08 16:24:06 +0530

    Fix database.py to read DATABASE_URL from environment

--- fintrack-backend/database.py
+++ fintrack-backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Jungkook198@db.wfdpzwafhdzafegketwi.supabase.co:5432/postgres"
+DATABASE_URL =  os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
