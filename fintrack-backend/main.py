52585ff58fd95a354729536b9acfe7ffe6f426a3
JyotiKanyal08 <jyotikanyal48@gmail.com>
2026-07-10 03:01:41 +0530

    Update main.py

--- fintrack-backend/main.py
+++ fintrack-backend/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
         "https://*.vercel.app"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
